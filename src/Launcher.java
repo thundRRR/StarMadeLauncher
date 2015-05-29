4db4ea9227e8e1d1b7a7c9dac7a3646618a80b02
daggad95 <daggad95@gmail.com>
2015-05-29 14:05:29 -0700

    Change to javafx

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,15 +1,23 @@
-import javax.swing.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 /**
  * Created by David on 5/29/2015.
  */
-public class Launcher extends JFrame {
-    public Launcher() {
-        setSize(400, 400);
-        setVisible(true);
+public class Launcher extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 1280, 720);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    public static void main(String args[]) {
-        Launcher launcher = new Launcher();
+    public static void main(String[] args) {
+        launch(args);
     }
 }
