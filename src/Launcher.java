28e2f29330fc21472cf9c3392c58b7de49d69905
daggad95 <daggad95@gmail.com>
2015-05-29 13:16:11 -0700

    Added launcher class

---
+++ src/Launcher.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * Created by David on 5/29/2015.
+ */
+public class Launcher extends JFrame {
+    public Launcher() {
+        setSize(400, 400);
+        setVisible(true);
+    }
+
+    public static void main(String args[]) {
+        Launcher launcher = new Launcher();
+    }
+}
